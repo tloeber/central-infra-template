637d17ba52ee049dbabf8420cbb169b7dde477b1
tloeber <thomas.loeber73@gmail.com>
2024-03-26 08:32:07 -0400

    Specify tf version of workspaces to make sure they match version locally and on tf cloud. Terraform Databricks workspace.

--- terraform/centralized_infra/tfe_organizations.tf
+++ terraform/centralized_infra/tfe_organizations.tf
@@ -3,13 +3,26 @@ resource "tfe_organization" "my-org" {
   email = var.organization_email
 }
 
+locals {
+  # todo: share with version constraint for workspaces
+  terraform_version = "~> 1.6.6"
+}
+
 resource "tfe_workspace" "workspace" {
-  name           = "central-infra"
-  organization   = tfe_organization.my-org.name
-  queue_all_runs = false
+  name              = "central-infra"
+  organization      = tfe_organization.my-org.name
+  terraform_version = local.terraform_version
+  queue_all_runs    = false
   # vcs_repo {
   #   branch = "main"
   #   identifier     = "my-org-name/vcs-repository" # todo: define repo as TF and use output
   #   oauth_token_id = tfe_oauth_client.github.oauth_token_id
   # }
+}
+
+resource "tfe_workspace" "databricks-test" {
+  name              = "MLOps-template-dev"
+  organization      = tfe_organization.my-org.name
+  queue_all_runs    = false
+  terraform_version = local.terraform_version
 }
