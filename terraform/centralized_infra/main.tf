3b4e82aee8bb5618b3bed2ef364d4ddb1b013300
tloeber <thomas.loeber73@gmail.com>
2024-04-08 09:21:49 -0400

    Use my github  module from the external location

--- terraform/centralized_infra/main.tf
+++ terraform/centralized_infra/main.tf
@@ -1,7 +1,10 @@
 module "repo" {
-  source      = "./modules/github-repo"
-  name        = "central-infra-template"
-  description = "Template for centralized infrastructure"
-  visibility  = "public"
-  is_template = true
+  source           = "../../../terraform-github-repo"
+  repo_name        = "central-infra-template"
+  repo_description = "Template for centralized infrastructure"
+  repo_visibility  = "public"
+  is_template      = true
+
+  aws_region            = "us-east-1"
+  secret_arn_github_PAT = var.secret_arn_github_PAT
 }
--- terraform/centralized_infra/variables.tf
+++ terraform/centralized_infra/variables.tf
@@ -18,3 +18,8 @@ variable "github_owner" {
   description = "The owner of the GitHub repository"
   type        = string
 }
+
+variable "secret_arn_github_PAT" {
+  description = "The ARN of the secret containing the GitHub PAT for the repository"
+  type        = string
+}
